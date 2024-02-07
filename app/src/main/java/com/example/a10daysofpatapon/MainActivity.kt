631cfca5eb84c6a1f152ad9fec4c370555a197d5
BingPom <bingpom@mailfence.com>
2024-02-07 15:01:34 +0100

    First Commit
    Added Icon, Fonts and Logo

--- app/src/main/java/com/example/a10daysofpatapon/MainActivity.kt
+++ app/src/main/java/com/example/a10daysofpatapon/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.a10daysofpatapon.ui.theme._10DaysOfPataponTheme
+import com.example.compose._10DaysOfPataponTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
